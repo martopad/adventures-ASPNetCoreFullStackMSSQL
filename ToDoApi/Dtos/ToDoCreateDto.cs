d4d4b5b557cf27a0ebbd6bc737849a36d6c7f738
martopad <martopad1@gmail.com>
2020-05-24 16:30:07 +0800

    some data annotations to make error handling easier when user inputs an invalid create

--- ToDoApi/Dtos/ToDoCreateDto.cs
+++ ToDoApi/Dtos/ToDoCreateDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDoApi.Dtos
 {
     public class ToDoCreateDto
     {
+        [Required]
+        [MaxLength(140)]
         public string Name { get; set; }
+
+        [Required]
         public bool IsDone { get; set; }
 
         // public string Description { get; set; }
